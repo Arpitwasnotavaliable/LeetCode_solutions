1bd35a92fc12504ccddf5de7e002537a31d5cd56
Arpit <125184867+Arpitwasnotavaliable@users.noreply.github.com>
2026-07-16 13:22:57 +0530

    Time: 76 ms (50.08%) | Memory: 155.7 MB (75.58%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
@@ -1,21 +1,56 @@
+// class Solution {
+// public:
+//     long long gcdSum(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<long long> prefixGcd(n);
+//         long long mx = 0;
+//         for (int i = 0; i < n; i++) {
+//             mx = max(mx, (long long)nums[i]);
+//             prefixGcd[i] = __gcd((long long)nums[i], mx);
+//         }
+//         sort(prefixGcd.begin(), prefixGcd.end());
+//         long long sum = 0;
+//         int l = 0, r = n - 1;
+//         while (l < r) {
+//             sum += __gcd(prefixGcd[l], prefixGcd[r]);
+//             l++;
+//             r--;
+//         }
+//         return sum;
+//     }
+// };
+
+
 class Solution {
 public:
+    static inline int mygcd(int a, int b) {
+        while (b) {
+            int t = a % b;
+            a = b;
+            b = t;
+        }
+        return a;
+    }
+
     long long gcdSum(vector<int>& nums) {
         int n = nums.size();
-        vector<long long> prefixGcd(n);
-        long long mx = 0;
+        vector<int> prefixGcd(n);
+        int mx = 0;
         for (int i = 0; i < n; i++) {
-            mx = max(mx, (long long)nums[i]);
-            prefixGcd[i] = __gcd((long long)nums[i], mx);
+            if (nums[i] > mx) mx = nums[i];
+            prefixGcd[i] = mygcd(nums[i], mx);
         }
         sort(prefixGcd.begin(), prefixGcd.end());
         long long sum = 0;
         int l = 0, r = n - 1;
         while (l < r) {
-            sum += __gcd(prefixGcd[l], prefixGcd[r]);
+            sum += mygcd(prefixGcd[l], prefixGcd[r]);
             l++;
             r--;
         }
         return sum;
     }
 };
+
+
+
