70dede63230463aeea564d7c12026464dd23c50c
Arpit <125184867+Arpitwasnotavaliable@users.noreply.github.com>
2026-09-18 12:34:15 +0530

    Time: 8 ms (78.98%) | Memory: 23.2 MB (88.07%) - LeetSync

---
+++ 1644-maximum-number-of-non-overlapping-substrings/maximum-number-of-non-overlapping-substrings.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<string> maxNumOfSubstrings(string s) {
+        int n = s.size();
+
+        vector<int> first(26, n);
+        vector<int> last(26, -1);
+
+        // Find first and last occurrence
+        for (int i = 0; i < n; i++) {
+            int c = s[i] - 'a';
+            first[c] = min(first[c], i);
+            last[c] = i;
+        }
+
+        vector<pair<int, int>> intervals;
+
+        // Create the smallest valid interval for each character
+        for (int c = 0; c < 26; c++) {
+            if (last[c] == -1)
+                continue;
+
+            int l = first[c];
+            int r = last[c];
+            bool valid = true;
+
+            for (int i = l; i <= r; i++) {
+                int x = s[i] - 'a';
+
+                if (first[x] < l) {
+                    valid = false;
+                    break;
+                }
+
+                r = max(r, last[x]);
+            }
+
+            if (valid)
+                intervals.push_back({l, r});
+        }
+
+        // Sort by ending position
+        sort(intervals.begin(), intervals.end(),
+             [](auto &a, auto &b) {
+                 return a.second < b.second;
+             });
+
+        vector<string> ans;
+        int end = -1;
+
+        // Greedily choose intervals that don't overlap
+        for (auto [l, r] : intervals) {
+            if (l > end) {
+                ans.push_back(s.substr(l, r - l + 1));
+                end = r;
+            }
+        }
+
+        return ans;
+    }
+};
